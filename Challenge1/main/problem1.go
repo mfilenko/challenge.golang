0e1d2356ca5b35a2b5b1f2fcb523c925e11f6b0b
Max Filenko <contact@filenko.ms>
2019-04-04 15:55:08 +0200

    Problem 1: limit the output :checkered_flag:
    There are couple of ways to manage concurrency. Channels are the most
    common one.
    
    Our problem falls exactly under this pattern.
    
    Essentially, there are two ways to deal with goroutines here.
    
    The first one is to communicate from a goroutine to the main thread
    each time a random number has been generated and stop when we gained
    enough data. In this scenario we have to listen on the channel our
    goroutines write to which means main goroutine will be blocked since
    reading from a channel is a blocking operation.
    
    The second approach is about the same idea, but different
    communication direction: main function defines all necessary input for
    goroutines and then let them go to do their job. In this case we don't
    have to block main thread every time it has to read goroutines'
    result.
    
    I went this way in my solution.
    
    In order to implement it I set up a buffered channel `c` which is also
    known as asynchronous or non-blocking channel and pushed the amount of
    random numbers we need to get generated by all goroutines. Then, when
    all goroutines are fired, they start to exhaust this channel one by
    one until there's nothing left. Once this is done a `default`
    communication switch gets triggered which simply returns from a
    goroutine.

--- Challenge1/main/problem1.go
+++ Challenge1/main/problem1.go
@@ -7,22 +7,18 @@ import (
 )
 
 func problem1() {
+	const limit = 100
+	c := make(chan int, limit)
 
 	log.Printf("problem1: started --------------------------------------------")
 
-	//
-	// Todo:
-	//
-	// Quit all go routines after
-	// a total of exactly 100 random
-	// numbers have been printed.
-	//
-	// Do not change the 25 in loop!
-	//
+	for i := 0; i < limit; i++ {
+		c <- i
+	}
 
 	for inx := 0; inx < 10; inx++ {
 
-		go printRandom1(inx)
+		go printRandom1(inx, c)
 
 	}
 
@@ -39,15 +35,13 @@ func problem1() {
 	log.Printf("problem1: finised --------------------------------------------")
 }
 
-func printRandom1(slot int) {
-
-	//
-	// Do not change 25 into 10!
-	//
-
+func printRandom1(slot int, c <-chan int) {
 	for inx := 0; inx < 25; inx++ {
-
-		log.Printf("problem1: slot=%03d count=%05d rand=%f", slot, inx, rand.Float32())
-
+		select {
+		case <-c:
+			log.Printf("problem1: slot=%03d count=%05d rand=%f", slot, inx, rand.Float32())
+		default:
+			return
+		}
 	}
 }
